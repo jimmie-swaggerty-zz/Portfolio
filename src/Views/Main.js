70c501b4e8783ee24d42db0434de44ce200fcc7f
jimmie-swaggerty <jimmie.swaggerty@gmail.com>
2021-09-24 14:28:37 -0500

    more updates

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -1,14 +1,21 @@
+import { faAws, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import MainBanner from '../Components/MainBanner'
 import NavBar from '../Components/NavBar'
+import mongodb from '../images/mongodb.png'
+import wix from '../images/wix.png'
+import jquery from '../images/jquery.png'
+import mysql from '../images/mysql.png'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const Main = () => {
         return (
                 <div className="">
                         <MainBanner />
                         <NavBar />
-                        <div className="container-flex">
-                                <div className="d-flex p-2">
+                        <div className="container-fluid">
+                                <div className="d-flex row p-2">
                                         <div className="col-lg-4 col-12 text-center">
                                                 <h1 className="section-header">My Story</h1>
                                         </div>
@@ -18,24 +25,51 @@ const Main = () => {
                                                                 My passion for <div className="highlight">web development</div>  started in 6th grade when my tech teacher introduced me to basic HTML. That year, I competed against my upperclassmen in a timed code off and won!
                                                         </p>
                                                         <p>
-                                                                As a child of the MySpace era, I began to learn CSS to assist my friends in customizing their profiles. I was completely obsessed with the different aspects of design that I could accomplish with simple strings of code!
+                                                                As a child of the MySpace era, I began to learn <div className="highlight">CSS</div> to assist my friends in customizing their profiles. I was completely obsessed with the different aspects of design that I could accomplish with simple strings of code!
                                                         </p>
                                                         <p>
-                                                                As an adult, I put my focus into entrepreneurship, doing everything from business adminstration to graphic and web design. This eventually led me to Wix, where I got my initial introduction to JQuery and their custom language, Velo. I have been designing with Wix for over 10 years, but I was desperate to learn more.
+                                                                As an adult, I put my focus into entrepreneurship, doing everything from business adminstration to <div className="highlight">graphic design</div> and <div className="highlight">web development</div>. This eventually led me to Wix, where I got my initial introduction to <div className="highlight">JQuery</div> and their custom language, Velo. I have been designing with Wix for over 10 years, but I was desperate to learn more.
                                                         </p>
                                                         <p>
-                                                                In the Spring of 2020 I decided to enroll in Coding Dojo for a self-paced course in the MERN stack. I became enthralled in the different ways you can develop using languages like React, Node.js, MongoDB, and Express. I decided to continue my education my enrolling in the part time bootcamp in Winter of 2020 to get a more one on one experience learning development.
+                                                                In the Spring of 2020 I decided to enroll in Coding Dojo for a self-paced course in the MERN stack. I became enthralled in the different ways you can develop using languages like <div className="highlight">React</div>, <div className="highlight">Node.js</div>, <div className="highlight">MongoDB</div>, and <div className="highlight">Express</div>. I decided to continue my education my enrolling in the part time bootcamp in Winter of 2020 to get a more one on one experience learning development.
                                                         </p>
                                                 </div>
                                         </div>
                                 </div>
-                                <div className="d-flex p-2">
+                                <div className="d-flex row p-2">
                                         <div className="col-lg-4 col-12 text-center">
                                                 <h1 className="section-header">My Toolkit</h1>
                                         </div>
-                                        <div className="col-lg-8 col-12 m-2">
-                                                <div className="text-body">
-                                                        <p>test</p>
+                                        <div className="col-lg-8 col-12">
+                                                <div className="text-body d-flex justify-content-around">
+                                                        <div className="col text-teal text-center">
+                                                                <FontAwesomeIcon icon={faReact} className="fa-3x" />
+                                                                <br />React
+                                                        </div>
+                                                        <div className="col text-teal text-center">
+                                                                <FontAwesomeIcon icon={faNodeJs} className="fa-3x text-teal" />
+                                                                <br />Node.js
+                                                        </div>
+                                                        <div className="col text-teal text-center">
+                                                                <FontAwesomeIcon icon={faAws} className="fa-3x text-teal" />
+                                                                <br />Amazon AWS
+                                                        </div>
+                                                        <div className="col text-teal text-center">
+                                                                <img src={mongodb} className="icon" />
+                                                                <br />MongoDB
+                                                        </div>
+                                                        <div className="col text-teal text-center">
+                                                                <img src={mysql} className="icon" />
+                                                                <br />MySQL
+                                                        </div>
+                                                        <div className="col text-teal text-center">
+                                                                <img src={jquery} className="icon" />
+                                                                <br />JQuery
+                                                        </div>
+                                                        <div className="col text-teal text-center">
+                                                                <FontAwesomeIcon icon={faPlus} className="fa-3x text-teal" />
+                                                                <br />and more!
+                                                        </div>
                                                 </div>
                                         </div>
                                 </div>
